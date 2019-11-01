65c1c9b5df4f3c8bd87f1dab0a7f6268b56b9b28
James Woolfenden <james.woolfenden@gmail.com>
2019-11-01 16:15:59 +0000

    revised

--- example/exampleA/provider.gcp.tf
+++ example/exampleA/provider.gcp.tf
@@ -1,13 +1,13 @@
-provider "google" {
+provider google {
   project = "focused-elysium-224508"
   region  = "europe-west1"
-  version = "2.10"
+  #version = "2.10"
 }
 
 provider "local" {
-  version = "1.3"
+  #version = "1.3"
 }
 
 provider "template" {
-  version = "2.1"
+  #version = "2.1"
 }
--- examples/exampleA/module.statebucket.tf
+++
@@ -1,5 +0,0 @@
-module "statebucket" {
-  source      = "JamesWoolfenden/statebucket/gcp"
-  version     = "0.0.3"
-  common_tags = "${var.common_tags}"
-}
